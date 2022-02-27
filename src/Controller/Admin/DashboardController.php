749b87afa981e9e1abe7a21efaf9ca02870d7bc1
benyoubhiba <benyoub.hiba2000@gmail.com>
2022-02-27 21:37:53 +0000

    Updated crud-user

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -15,7 +15,7 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        return $this->render('bundles/EasyAdminBundle/welcome.html.twig');
     }
 
     public function configureDashboard(): Dashboard
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use  EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +18,36 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+    
+public function configureActions(Actions $actions): Actions
+{
+return $actions
+->add(Crud::PAGE_INDEX,Action::DETAIL)
+->update (Crud::PAGE_INDEX,Action::NEW,function(Action $action){
+    return $action->setIcon('fa fa-user');
+    })
+ ->update (Crud::PAGE_INDEX,Action::EDIT,function(Action $action){
+        return $action->setIcon('fa fa-edit');
+        })
+ ->update (Crud::PAGE_INDEX,Action::DETAIL,function(Action $action){
+            return $action->setIcon('fa fa-eye');
+            })
+ ->update (Crud::PAGE_INDEX,Action::DELETE,function(Action $action){
+                return $action->setIcon('fa fa-trash');
+                });
+
+}
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('username'),
+            TextField::new('cin'),
+             EmailField::new('email'),
+            TextField::new('password')->hideOnIndex(),
+           
+            
         ];
     }
-    */
+    
 }
